4788b9fa3d4629a1013e199205026941280fdb9e
MrRiegel <ichhabads@gmail.com>
2016-02-05 00:24:06 +0100

    build 1.4

--- src/main/java/mrriegel/storagenetwork/StorageNetwork.java
+++ src/main/java/mrriegel/storagenetwork/StorageNetwork.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = StorageNetwork.MODID, name = StorageNetwork.MODNAME, version = StorageNetwork.VERSION, guiFactory = "mrriegel.storagenetwork.config.GuiFactory")
 public class StorageNetwork {
 	public static final String MODID = "storagenetwork";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.4";
 	public static final String MODNAME = "Storage Network";
 
 	@Instance(StorageNetwork.MODID)
